10fd3183147ca28630e0beb2395c21d8a0c0551f
Caleb Huitt <development-caleb@huitt.org>
2011-12-11 09:20:04 -0600

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LovingCritique::Application.config.secret_token = '1df80ad9d7792317bdeb0aec5a65d541c729dfc0eca1e5ab91f4aeb208a45fd74f68a6d4c491f2f45dcc22bd7a4b65bc7ba1fdbd5550f60f5c1a863232239836'
